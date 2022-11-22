4d9c7b6d24aa164cb14a4694f4b66f0eac4d172f
Eric Bowser <ericbo@engineer-it-all.com>
2022-11-21 23:06:08 -0700

    env file for port and NODE_ENV

---
+++ server.js
@@ -0,0 +1,39 @@
+﻿const express = require('express')
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+// const server = require('./server');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+console.log('passed port to use', port);
+const app = express();
+// app.use(server);
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Calc Front End",
+            version: "1.0.0",
+            description: "Login and Calc API Front-End",
+            contact: {
+                name: "API Support",
+                url: "https://erb-tech.com",
+                email: "eric@erb-tech.com",
+            },
+        },
+
+        servers: [
+            {
+                url: "http://localhost:5000",
+                description: "My API Documentation",
+            },
+        ],
+    },
+    apis: ['./docs/openapi_3.yaml'],
+}
+
+const specs = swaggerJsdoc(options);
+app.use("/", swaggerUI.serve, swaggerUI.setup(specs));
+app.listen(port, () => console.log(`Listening on port ${port}`))
