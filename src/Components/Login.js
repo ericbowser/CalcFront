69adaa129a6a3aa3cef29686549766f2f08b6d01
ericbowser <eric@erb-tech.com>
2022-12-09 04:23:54 -0700

    Login page and route

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,60 @@
-﻿import React from "react";
-import {Form, Container} from 'react-bootstrap';
+﻿import React, {useState, useEffect} from 'react'
+import { Form, Container, Button } from 'react-bootstrap'
+// import {isEmpty} from 'lodash'
 
-function Login() {
-    return (
-        <Container fluid>
-            <Form >
-                <Form.Label title={'username'}>User Name:</Form.Label>
-                <Form.Control title={'username'} id={'username'}>
-                </Form.Control>
-            </Form>
-        </Container>
-    )
-};
+function Login () {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  
+  useEffect(() => {
+  }, [password, userName])
+  
+  return (
+    <Container fluid>
+      <Form onSubmit
+      <Button variant='primary' type='submit'  onSubmit={(event) => {
+        try {
+          console.log('username', event.target.value);
+          console.log('password', event.target.value.username);
+        } catch (e) {
+          console.log(e.target.value);
+          // handle your error state here
+        }
+      }}>
 
-export default Login;
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type='userName' placeholder='Enter email' onChange={event => {
+              setUserName(event.target.value);
+          }}/>
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' onChange={event => {
+            setPassword(event.target.value);
+          }}/>
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+          <Form.Check type='checkbox' label='Check me out' />
+        </Form.Group>
+        <Button variant='primary' type='submit'  onSubmit={(event) => {
+          try {
+            console.log('username', event.target.value);
+            console.log('password', event.target.value.username);
+          } catch (e) {
+            console.log(e.target.value);
+            // handle your error state here
+          }
+        }}>
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  )
+}
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,44 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-    Link,
-} from "react-router-dom";
-import Login from './Components/Login';
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link
+} from 'react-router-dom'
+import { Alert, Container } from 'react-bootstrap'
+import Login from './Components/Login'
 
 const router = createBrowserRouter([
-    {
-        path: "/Login",
-        element: (
-            <Login>
-                <h1>Hello World</h1>
-                <Link to="about">About Us</Link>
-            </Login>
-        )
-    },
-    {
-        path: "/",
-        element: (
-            <div>Home page</div>        
-        )
-    },
-    {
-        path: "about",
-        element: <div>About</div>,
-    }]);
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/calcAccount',
+    element: <div>TODO -- Calc account after logged in</div>
+  },
+  {
+    path: '/',
+    element: (
+      <Container fluid>
+        <Alert variant={'info'}>
+          <h3>
+            This is a simple calculator app that performs basic math, and a
+            Random function API call to ...
+            <div>
+              <ul>
+                <br />
+                <li>To login, type /login into the browser</li>
+              </ul>
+            </div>
+          </h3>
+        </Alert>
+      </Container>
+    )
+  }
+])
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.Fragment>
-        <RouterProvider router={router}>
-            <React.StrictMode>
-                <App/>
-            </React.StrictMode>
-        </RouterProvider>
-    </React.Fragment>
-);
+  <React.Fragment>
+    <RouterProvider router={router}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </RouterProvider>
+  </React.Fragment>
+)
 
-export default root;
+export default root
